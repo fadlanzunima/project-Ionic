09b1cea78525aec11709dc8e5dc3701647da0820
fadlanzunima <fadlanzunima@gmail.com>
2017-05-17 20:32:31 +0700

    project ionic

--- src/pages/syarat-pendaftaran/syarat-pendaftaran.ts
+++ src/pages/syarat-pendaftaran/syarat-pendaftaran.ts
@@ -12,11 +12,29 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'syarat-pendaftaran.html'
 })
 export class SyaratPendaftaranPage {
+  shownGroup = null;
+  diseases = [
+    { title: "Syarat Jalur SNMPTN", description: "Type 1 diabetes is an autoimmune disease in which the body’s immune system attacks and destroys the beta cells in the pancreas that make insulin." },
+    { title: "Syarat Jalur SBMPTN", description: "Multiple sclerosis (MS) is an autoimmune disease in which the body's immune system mistakenly attacks myelin, the fatty substance that surrounds and protects the nerve fibers in the central nervous system." },
+    { title: "Syarat Jalur Mandiri", description: "Syaratnya liat di PTN masing-masing." }
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SyaratPendaftaranPage');
+  }
+
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+        this.shownGroup = null;
+    } else {
+        this.shownGroup = group;
+    }
+  }
+
+  isGroupShown(group) {
+      return this.shownGroup === group;
   }
 
 }
